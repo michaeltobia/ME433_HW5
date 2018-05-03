a5132ef5b1f0fd6988c219e07e29f075b47f3e9e
Michael Tobia <michaeltobia2019@u.northwestern.edu>
2018-05-03 00:38:25 -0500

    final commit

--- Mike_HW5.X/main.c
+++ Mike_HW5.X/main.c
@@ -37,7 +37,7 @@
 #pragma config FUSBIDIO = 0b1 // USB pins controlled by USB module
 #pragma config FVBUSONIO = 0b1 // USB BUSON controlled by USB module
 
-#define SLAVE_ADDR 0x20 // 0b0
+#define SLAVE_ADDR 0x20
 
 void initExpander(char);
 void setExpander(char, char);
@@ -69,52 +69,22 @@ int main() {
 
     __builtin_enable_interrupts();
     
-    LATAbits.LATA4 = 1;
-    initExpander(0b10000000);
-    unsigned char cur_GPIO = 0, not_but_press = 0;
+    initExpander(0b10000000);   // init expander w/ only GP7 as input
     
+    unsigned char cur_GPIO = 0, not_but_press = 0;
     while(1){
         _CP0_SET_COUNT(0);                  // reset clock for 1kHz loop
-//        
-        cur_GPIO = getExpander(0x09);  // get GPIO register
-//        i2c_master_start();
-//        i2c_master_send(SLAVE_ADDR << 1 | 0);
-//        i2c_master_send(0x09);
-//        i2c_master_restart();
-//        i2c_master_send(SLAVE_ADDR << 1 | 1);
-//        cur_GPIO = i2c_master_recv();
-//        i2c_master_ack(1);
-//        i2c_master_stop();
-        not_but_press = (cur_GPIO >> 7);          // right shift GPIO read by 7 to obtain button press status
-        setExpander(0,!not_but_press);
-//        if(!not_but_press){                      // if GP7 is low (button pressed)
-//            setExpander(0,1);                           // GP0 high (LED on)
-////            i2c_master_start();
-////            i2c_master_send(SLAVE_ADDR << 1 | 0);
-////            i2c_master_send(0x0A);
-////            i2c_master_send(0x00);
-////            i2c_master_stop();
-//        }
-//        else{                                   // if GP7 is high (button released)
-//            setExpander(0,0);                          // GP0 high (LED on)
-////            LATAINV = 0x10;
-////            i2c_master_start();
-////            i2c_master_send(SLAVE_ADDR << 1 | 0);
-////            i2c_master_send(0x0A);
-////            i2c_master_send(0x01);                           // GP0 low (LED off)
-////            i2c_master_stop();
-//        }
-//        setExpander(0,0);
-    
+
+        cur_GPIO = getExpander(0x09);       // get GPIO register
+
+        if((getExpander(0x09)>>7)==1){      // if GP7 is high
+            setExpander(0,0);                   // set GP0 low
+        }else{                              // if GP7 is low
+            setExpander(0,1);                   // set GP0 high
+        }
         while(_CP0_GET_COUNT() < 24000000/1000){;} // do nothing until 1ms passes
-        LATAINV = 0x10;
+        LATAINV = 0x10;                     // invert USER LED signal
     }
-    
-    
-//    setExpander(0,1);
-//    while(1){;}
-//    
-    
 }
 
 void initExpander(char iodir_set) {
@@ -141,71 +111,16 @@ void initExpander(char iodir_set) {
     i2c_master_send(0x06);
     i2c_master_send(iodir_set);
     i2c_master_stop();
-    
-//    // Set OLAT register
-//    i2c_master_start();
-//    i2c_master_send(SLAVE_ADDR << 1 | 0);
-//    i2c_master_send(0x0A);
-//    i2c_master_send(0x00);
-//    i2c_master_stop();
 }
 
 void setExpander(char pin, char level) {    
-    // Read current GPIO
-    i2c_master_start();                   // send start
-    i2c_master_send(SLAVE_ADDR << 1 | 0); // send slave opp (w/ write)
-    i2c_master_send(0x09);                // send GPIO address to read
-    i2c_master_restart();                 // send restart
-    i2c_master_send(SLAVE_ADDR << 1 | 1); // send slave opp (w/ read)
-    char cur_GPIO = i2c_master_recv();    // receive byte from slave
-    i2c_master_ack(1);                    // send ack to confirm receive
-    i2c_master_stop();                    // send stop
-    
-    char compare = ((cur_GPIO >> pin))&(level);
-    char output_message = 0;
-    
-    if(level){
-        char output_message = cur_GPIO | (0x01 << pin);
-    }else if(!level){
-        char output_message = cur_GPIO & !(0x01 << pin);
-    }
+    char output_message = level << pin;
     // Send message to OLAT
     i2c_master_start();                    // send start
     i2c_master_send(SLAVE_ADDR << 1 | 0);  // send slave opp (w/ write)
     i2c_master_send(0x0A);                 // send OLAT address
     i2c_master_send(output_message);       // send message to OLAT
-    i2c_master_stop();                     // send stop
-//    if(compare != 0b1){
-//        if(level){
-//            char output_message = cur_GPIO | (0x01 << pin);
-//        }else if(!level){
-//            char output_message = cur_GPIO & ~(0x01 << pin);
-//            
-//        }
-//        // Send message to OLAT
-//        i2c_master_start();                    // send start
-//        i2c_master_send(SLAVE_ADDR << 1 | 0);  // send slave opp (w/ write)
-//        i2c_master_send(0x0A);                 // send OLAT address
-//        i2c_master_send(output_message);       // send message to OLAT
-//        i2c_master_stop();                     // send stop
-//    }else{
-//        return;
-//    }
-    
-    
-//    if(level){          // if level is high
-//        output_message = (level << pin) | 0x00;     // shift 'level' by 'pin' bits
-//        // XOR pin+level with current GPIO to preserve old GPIO while setting pin
-//        output_message = cur_GPIO ^ output_message; 
-//        
-//    }else if(!level){   // if level is low
-//        output_message = (!level << pin) | 0x00;    // shift not 'level' by 'pin' bits
-//        // XOR pin+!level with current GPIO to preserve old GPIO while setting pin
-//        output_message = cur_GPIO ^ output_message;
-//    }
-//    
-
-    
+    i2c_master_stop();                     // send stop   
 }
 
 unsigned char getExpander(char reg_add) {
